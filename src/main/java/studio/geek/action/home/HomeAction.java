733e722e518688ef9d41cdc821fb4959044595c7
thinkforfree <lwhyx9@qq.com>
2017-03-08 19:38:11 +0800

    解决跨域问题

--- src/main/java/studio/geek/action/home/HomeAction.java
+++ src/main/java/studio/geek/action/home/HomeAction.java
@@ -1,10 +1,7 @@
 package studio.geek.action.home;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import studio.geek.entity.Candidate;
 import studio.geek.entity.Member;
 import studio.geek.entity.Production;
